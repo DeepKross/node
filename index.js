941631c566a02bb45d6cf579e8e99ca8dab541a4
Mike Tanchuk <volkermischa@gmail.com>
2023-05-04 14:55:17 +0300

    Data Validation provided. Css added. Html rendering templates added. Path existment validation added. Basic error handlers added.

--- index.js
+++ index.js
@@ -3,41 +3,76 @@ const path = require("path");
 const app = express();
 const fs = require("fs");
 const readline = require("readline");
+
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use('/assets', express.static('assets'));
 const PORT = process.env.PORT || 3000;
-
-const url_to_test_data = `D:\\LookingForJob\\TMacademy\\node\\data\\data.json`;
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-function getJsonPath() {
-    let path;
-    rl.question('Please enter url to json file: ', (input) => {
-        path = input
-        console.log(input);
-        rl.close();
-    });
-    return path;
+function isValidData(data) {
+    const requiredKeys = [
+        'title',
+        'year',
+        'director',
+        'actors',
+        'poster',
+        'plot'
+    ];
+
+    for (const key of requiredKeys) {
+        if (!data.hasOwnProperty(key)) {
+            return false;
+        }
+    }
+
+    return Array.isArray(data.actors);
+
+
 }
 
-function getJsonData() {
-    let path = getJsonPath();
+function getJsonPath(callback) {
+    function promptForPath() {
+        rl.question('Please enter absolute path to json file: ', (input) => {
+            if (fs.existsSync(input)) {
+                let data = getJsonData(input);
+                if (isValidData(data)) {
+                    console.log('File path:', input);
+                    rl.close();
+                    callback(input);
+                } else {
+                    console.log('Invalid data format. Please try again.');
+                    promptForPath();
+                }
+            } else {
+                console.log('File not found. Please try again.');
+                promptForPath();
+            }
+        });
+    }
 
+    promptForPath();
+}
+
+function getJsonData(path) {
     let raw_data = fs.readFileSync(path, "utf8");
     return JSON.parse(raw_data);
 }
 
-app.get("/", (req, res) => {
-    let data = getJsonData();
+getJsonPath((path) => {
+    app.get("/", (req, res) => {
+        let data = getJsonData(path);
 
-    res.render("index", data);
+
+
+        res.render("index", data);
+    });
+
+    app.listen(PORT, () => {
+        console.log(`server started on port ${PORT}`);
+    });
 });
-
-app.listen(PORT, () => {
-    console.log(`server started on port ${PORT}`);
-});
-
