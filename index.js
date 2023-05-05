20672250efef154d603ba7eccfd19024a6eae116
Mike Tanchuk <volkermischa@gmail.com>
2023-05-05 11:11:57 +0300

    Changed data and added ability to enter path to ejs file.

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const readline = require("readline");
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use('/assets', express.static('assets'));
 const PORT = process.env.PORT || 3000;
 
 const rl = readline.createInterface({
@@ -14,40 +13,46 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-function isValidData(data) {
-    const requiredKeys = [
-        'title',
-        'year',
-        'director',
-        'actors',
-        'poster',
-        'plot'
-    ];
+function isValidData(data, requiredKeys) {
 
     for (const key of requiredKeys) {
         if (!data.hasOwnProperty(key)) {
             return false;
         }
     }
+}
 
-    return Array.isArray(data.actors);
+function promptForTemplatePath(callback) {
+    rl.question('Please enter absolute path to EJS template file: ', (input) => {
+        if (fs.existsSync(input)) {
+            console.log('Template path:', input);
+            callback(input);
+        } else {
+            console.log('Template not found. Please try again.');
+            promptForTemplatePath(callback);
+        }
+    });
+}
 
+function getTemplateVariables(templatePath) {
+    const ejsVariableRegex = /<%[=-]\s*([\w$]+)\s*%>/g;
+    const templateContent = fs.readFileSync(templatePath, "utf8");
+    const variables = [];
+    let match;
 
+    while ((match = ejsVariableRegex.exec(templateContent)) !== null) {
+        variables.push(match[1]);
+    }
+
+    return variables;
 }
 
 function getJsonPath(callback) {
     function promptForPath() {
         rl.question('Please enter absolute path to json file: ', (input) => {
             if (fs.existsSync(input)) {
-                let data = getJsonData(input);
-                if (isValidData(data)) {
-                    console.log('File path:', input);
-                    rl.close();
-                    callback(input);
-                } else {
-                    console.log('Invalid data format. Please try again.');
-                    promptForPath();
-                }
+                console.log('File path:', input);
+                callback(input);
             } else {
                 console.log('File not found. Please try again.');
                 promptForPath();
@@ -60,19 +65,32 @@ function getJsonPath(callback) {
 
 function getJsonData(path) {
     let raw_data = fs.readFileSync(path, "utf8");
+    console.log("Get Json Data");
     return JSON.parse(raw_data);
+
 }
 
-getJsonPath((path) => {
-    app.get("/", (req, res) => {
-        let data = getJsonData(path);
+getJsonPath((jsonPath) => {
+    promptForTemplatePath((templatePath) => {
+        const requiredKeys = getTemplateVariables(templatePath);
+        const jsonData = getJsonData(jsonPath);
 
+        console.log(requiredKeys);
+        console.log(jsonData);
 
+        if (isValidData(jsonData, requiredKeys)) {
+            app.get("/", (req, res) => {
+                res.render(templatePath, jsonData);
+            });
 
-        res.render("index", data);
-    });
-
-    app.listen(PORT, () => {
-        console.log(`server started on port ${PORT}`);
+            app.listen(PORT, () => {
+                console.log(`server started on port ${PORT}`);
+            });
+            rl.close();
+        } else {
+            console.log('The provided JSON data does not match the required variables in the template.');
+            rl.close();
+            console.log('Please restart the application and try again.');
+        }
     });
 });
